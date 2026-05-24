c9f7e4f95c90c9ad78a29f3bc57acd948b3ccf83
Frederic Triplett, Jr. <frederic.triplett@gmail.com>
2026-05-23 20:46:05 -0400

    Challenge 5 Completed with a README.md update.

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Submitted by: Frederic Triplett
 
 **Name of your app** is a website for the company Sea Monster Crowdfunding that displays information about the games they have funded.
 
-Time spent: **X** hours spent in total
+Time spent: **5** hours spent in total
 
 ## Required Features
 
@@ -25,20 +25,24 @@ Here's a walkthrough of implemented features:
 
 <img src='http://i.imgur.com/link/to/your/gif/file.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
 
-<!-- Replace this with whatever GIF tool you used! -->
-GIF created with ...  
-<!-- Recommended tools:
-[Kap](https://getkap.co/) for macOS
-[ScreenToGif](https://www.screentogif.com/) for Windows
-[peek](https://github.com/phw/peek) for Linux. -->
+GIF created with [OBS Studio](https://obsproject.com).
+
 
 ## Notes
+Challenges 0-2 were a great orientation into the project and refresher for web development. 
 
-Describe any challenges encountered while building the app.
+Challenge 3 was enjoyable because I felt my HTML fundamentals returning after not using them in awhile. I enjoyed looking back through the JavaScript documentation to get familiar with the DOM again and using the template literals to manipulate the contents of the DOM. 
+
+I was never good with arrow functions so I struggled a bit with Challenge 4, but after slowing down and wrestling a bit with `reduce()`, I feel more confident about using them in place of traditional functions. I understood their flexibility before, but I preferred using traditional functions - it felt more readable to me. 
+
+I got stumped for a bit on challenge 5 due to a funny little error of adding the parentheses to the function name in the `addEventListener` method for my buttons. I was wondering what was wrong with my code for a good minutes before I realized that was the error. Glad I didn't restart my code from the end of Challenge 4!
+
+
+
 
 ## License
 
-    Copyright 2026 [name of copyright owner]
+    Copyright 2026 Frederic Triplett, Jr (ftripjr.dev)
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
--- index.js
+++ index.js
@@ -36,7 +36,6 @@ function addGamesToPage(games) {
         // add the class game-card to the list
         gamecard.classList.add("game-card");
     
-        
         // set the inner HTML using a template literal to display some info 
         // about each game
         // TIP: if your images are not displaying, make sure there is space
@@ -108,10 +107,13 @@ function filterUnfundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have not yet met their goal
-
+    let unfundedGames = GAMES_JSON.filter( (game) => {
+        return game.pledged < game.goal;
+    });
 
     // use the function we previously created to add the unfunded games to the DOM
-
+    console.log(`Unfunded:  ${ unfundedGames.length }`);
+    addGamesToPage(unfundedGames);
 }
 
 // show only games that are fully funded
@@ -119,10 +121,12 @@ function filterFundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have met or exceeded their goal
-
+    let fundedGames = GAMES_JSON.filter( (game) => {
+        return game.pledged > game.goal;
+    });
 
     // use the function we previously created to add unfunded games to the DOM
-
+    console.log(`Funded:  ${ fundedGames.length }`);    addGamesToPage(fundedGames);
 }
 
 // show all games
@@ -130,7 +134,8 @@ function showAllGames() {
     deleteChildElements(gamesContainer);
 
     // add all games from the JSON data to the DOM
-
+    console.log(`Total:  ${ GAMES_JSON.length }`);
+    addGamesToPage(GAMES_JSON);
 }
 
 // select each button in the "Our Games" section
@@ -139,7 +144,9 @@ const fundedBtn = document.getElementById("funded-btn");
 const allBtn = document.getElementById("all-btn");
 
 // add event listeners with the correct functions to each button
-
+unfundedBtn.addEventListener("click", filterUnfundedOnly);
+fundedBtn.addEventListener("click", filterFundedOnly);
+allBtn.addEventListener("click", showAllGames);
 
 /*************************************************************************************
  * Challenge 6: Add more information at the top of the page about the company.
